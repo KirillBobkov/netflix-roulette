49af6b4772604a3fbc4196c09c706b1f44ba7375
Kirill Bobkov <kirill_bobkov@epam.com>
2020-06-11 22:15:19 +0000

    Task2

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,60 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Webpack = require('webpack');
+
+const isDev = process.env.NODE_ENV === 'development';
+const modeName = process.env.NODE_ENV;
+
+const cssLoaders = (extension) => {
+  const loaderConfig = [
+      {
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          hmr: isDev,
+          reloadAll: true
+        }
+      },
+      'css-loader', 
+  ];
+
+  extension && loaderConfig.push(extension);
+  return loaderConfig;
+}
 
 module.exports = {
+    mode: modeName,
     context: path.join(__dirname, 'src'),
-    entry: './index.js',
-
+    entry: {
+      main: ['@babel/polyfill', './index.js']
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[hash].js',
         path: path.join(__dirname, 'dist'),
     },
-
     resolve: {
         extensions: ['.js', '.jsx']
     },
-
+    devtool: isDev ? 'source-map' : '',
+    devServer: {
+      port: 4000,
+      hot: isDev
+    },
+    plugins: [
+      new Webpack.optimize.ModuleConcatenationPlugin(),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css'
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        collapse: !isDev
+      }),
+    ],
     module: {
-        rules: [{
+      rules: [
+        {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
@@ -28,14 +67,27 @@ module.exports = {
               plugins: ['@babel/plugin-proposal-class-properties']
             }
           }
-        }]
-      },
-
-    watch: false,
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        })
-    ]
+        },
+        {
+          test: /\.css$/,
+          use: cssLoaders()
+        },
+        {
+          test: /\.scss$/,
+          use: cssLoaders('sass-loader')
+        },
+        {
+          test: /\.(ico|jpg|jpeg|png|gif|svg|ttf|woff|woff2)(\?.*)?$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          }
+        }
+      ]
+    },
 }
+
+   
+
