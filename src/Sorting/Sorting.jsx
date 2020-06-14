86fee3488cdab95f581192be69f364a6923cd839
KirillBobkov <kirill.bobkov.1995@gmail.com>
2020-06-14 22:45:38 +0400

    added match movies element

--- src/Sorting/Sorting.jsx
+++ src/Sorting/Sorting.jsx
@@ -7,15 +7,19 @@ export default class Sorting extends React.PureComponent {
         return (
             <div className='sorting'>
                 <div className='sorting__container'>
-                    <span className='sorting__sort-description'>Sort by</span>
-                    <Button 
-                    className='button button--choosen button--left-border'
-                    text={'Release date'} 
-                    />
-                    <Button 
-                    className='button button--right-border'
-                    text={'Rating'} 
-                    />      
+                   <span className='sorting__match-movies'>7 movies found</span>
+
+                    <p>
+                        <span className='sorting__sort-description'>Sort by</span>
+                        <Button 
+                        className='button button--choosen button--left-border'
+                        text={'Release date'} 
+                        />
+                        <Button 
+                        className='button button--right-border'
+                        text={'Rating'} 
+                        /> 
+                    </p>  
                 </div>  
             </div>
           );
