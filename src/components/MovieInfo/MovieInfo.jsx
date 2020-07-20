5aeb52f667ee15835d38f713624a543de302bbe2
Kirill Bobkov <Kirill_Bobkov@epam.com>
2020-07-20 21:07:50 +0400

    refactoring MovieInfo

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -2,46 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './MovieInfo.scss';
 import { connect } from 'react-redux';
-import { getYear } from '../../utils';
+import { getYear, getImage } from '../../utils';
 
-const MovieInfo = (props) => {
-  const { movie } = props;
+class MovieInfo extends React.PureComponent {
+  state = { source: 'https://via.placeholder.com/260x365/000000?text=Image+has+not+loaded' }
 
-  const year = getYear(movie.release_date);
+  componentDidMount() {
+    const { movie: { poster_path } } = this.props;
+    getImage(poster_path)
+      .then((url) => this.setState({ source: url }))
+      .catch((error) => console.log(error));
+  }
 
-  return (
-    <div className='header__movie'>
-      <p className='header__movie-poster'>
-        <img
-          src={movie.poster_path} width='260'
-          height='365' alt={movie.title}
-        />
-      </p>
-      <div className='header__movie-info'>
-        <h1 className='header__movie-title'>{movie.title}
-          <span className='header__movie-rating'>{movie.vote_average}</span>
-        </h1>
-       
-        <p className='header__movie-nomination'>{movie.tagline}</p>
-        <p className='header__movie-details'>
-          {year 
-            ? <span>
-              <span className='header__movie-year'>{year}&nbsp;</span>year &emsp;
-              </span>
-            : null}
+  render() {
+    const { movie } = this.props;
+    const year = getYear(movie.release_date);
+
+    return (
+      <div className='header__movie'>
+        <p className='header__movie-poster'>
+          <img
+            src={this.state.source} width='260'
+            height='365' alt={movie.title}
+          />
+        </p>
+        <div className='header__movie-info'>
+          <h1 className='header__movie-title'>{movie.title}
+            <span className='header__movie-rating'>{movie.vote_average}</span>
+          </h1>
          
-          {movie.runtime
-            ? <span>
-              <span className='header__movie-length'>{movie.runtime}&nbsp;</span>min
-              </span>
-            : null}
-        </p>
-        <p className='header__movie-description'>
-          {movie.overview}
-        </p>
+          <p className='header__movie-nomination'>{movie.tagline}</p>
+          <p className='header__movie-details'>
+            {year 
+              ? <span>
+                <span className='header__movie-year'>{year}&nbsp;</span>year &emsp;
+                </span>
+              : null}
+           
+            {movie.runtime
+              ? <span>
+                <span className='header__movie-length'>{movie.runtime}&nbsp;</span>min
+                </span>
+              : null}
+          </p>
+          <p className='header__movie-description'>
+            {movie.overview}
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 const mapStateToProps = (state, ownProps) => {
