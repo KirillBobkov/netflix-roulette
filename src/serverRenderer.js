6c8a8b8e9e9b5bbc74cdcda7fb98205ea3193c65
Kirill <Kirill_Bobkov@epam.com>
2020-08-09 16:41:52 +0400

    fixed serverRenderer

--- src/serverRenderer.js
+++ src/serverRenderer.js
@@ -5,21 +5,43 @@ import { StaticRouter } from 'react-router-dom';
 import configureStore from './store/store';
 import { getBundles } from 'react-loadable-ssr-addon';
 import Loadable from 'react-loadable';
+import manifest from '../dist/react-loadable-ssr-addon.json';
 
-const manifest = require('../dist/react-loadable-ssr-addon.json');
+const getTemplate = (html, state, scripts, styles) => (
+`
+  <!doctype html>
+  <html>
+    <head>
+      <meta charset=utf-8>
+      <title>React Server Side Rendering</title>
+      ${styles.map(style => {
+        return `<link href="/${style.file}" rel="stylesheet" />`;
+      }).join('\n')}
+    </head>
+    <body>
+      <div id="root">${html}</div>
+      <script>
+         window.PRELOADED_STATE = ${JSON.stringify(state).replace(/</g, '\\u003c')}
+      </script>
+      ${scripts.map(script => {
+        return `<script src="/${script.file}"></script>`;
+      }).join('\n')}
+    </body>
+  </html>
+`
+);
 
-export default function serverRenderer() {
+export const serverRenderer = () => {
   return (req, res) => {
     const store = configureStore();
     const context = {};
-    
     const modules = [];
 
     store
       .runSaga()
       .toPromise()
       .then(() => {
-        const htmlString = renderToString(
+        const template = renderToString(
           <Loadable.Capture report={moduleName => modules.push(moduleName)}>
             <App
               context={context}
@@ -28,37 +50,14 @@ export default function serverRenderer() {
               store={store}
             />
           </Loadable.Capture>
-          );
+        );
 
-        const bundles = getBundles(manifest, [...manifest.entrypoints, ...Array.from(modules)]);
+        const bundles = getBundles(manifest, [...manifest.entrypoints, ...modules]);
         const styles = bundles.css || [];
         const scripts = bundles.js || [];
-
         const preloadedState = store.getState();
 
-        res.send(
-        `
-        <!doctype html>
-        <html>
-          <head>
-            <meta charset=utf-8>
-            <title>React Server Side Rendering</title>
-            ${styles.map(style => {
-              return `<link href="/${style.file}" rel="stylesheet" />`;
-            }).join('\n')}
-          </head>
-          <body>
-            <div id="root">${htmlString}</div>
-            <script>
-               window.PRELOADED_STATE = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
-            </script>
-            ${scripts.map(script => {
-              return `<script src="/${script.file}"></script>`;
-            }).join('\n')}
-          </body>
-        </html>
-    `);
-
+        res.send( getTemplate(template, preloadedState, scripts, styles) );
     });
 
     // Do first render, starts initial actions.
@@ -71,8 +70,6 @@ export default function serverRenderer() {
           store={store}
         />
       </Loadable.Capture>);
-
-
 
     // When the first render is finished, send the END action to redux-saga.
     store.close();
